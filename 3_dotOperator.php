4d0075c4229d567edf946aac6ddd8b235356c639
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2022-03-12 16:28:34 +0300

    dot operator added

---
+++ 3_dotOperator.php
@@ -0,0 +1,9 @@
+$num1 = 40;
+$num2 = 10;
+$message = "Sum of numbers: ";
+$sum = $num1 + $num2;
+
+echo $message."".$sum."<br>";
+echo "Message data type: ".gettype($message)."<br>";
+echo "Sum data type: ".gettype($sum)."<br>";
+echo "Message data type: gettype($message)"."<br>";
