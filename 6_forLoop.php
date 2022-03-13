6775cacf5838b260e4aa25c977109c047fdc579e
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2022-03-13 17:56:08 +0300

    for loop

---
+++ 6_forLoop.php
@@ -0,0 +1,31 @@
+<?php
+
+for($i = 1; $i <= 10; $i++){
+
+    echo "Hi to everyone! $i <br>";
+
+    for($j = 1; $j <= 10; $j++){
+
+        echo "$j ";
+
+    }
+
+    echo "<br>";
+    /*if($i == 3){
+        break;
+    } */
+
+}
+
+echo "Loop completed.";
+
+echo "<br><br>**************************************<br><br>";
+
+
+for($k = 12; $k >= 2; $k-=2){
+
+    echo "$k<br>";
+
+} //12 10 8 6 4 2
+
+?>
