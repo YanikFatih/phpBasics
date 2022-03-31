16ed2337b1e9e897f86369e43609537a730ce566
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2022-03-31 13:59:27 +0300

    using textarea

--- 18_using_textarea.php
+++ 18_using_textarea.php
@@ -1,16 +1,24 @@
 <?php
 
+    // if($_POST){
+
+    //     $username = $_POST['username'];
+    //     $comment = $_POST['comment'];
+
+    //     echo $username."'s comment: ".$comment;
+
+    // }else{
+    //     echo "No comment found.";
+    // }
+    error_reporting(0);
+
     if($_POST){
-
-        $username = $_POST['username'];
-        $comment = $_POST['comment'];
-
+        $username = $_POST['username'] ? $_POST['username'] : null;
+        $comment = $_POST['comment'] ? $_POST['comment'] : null;
+    
         echo $username."'s comment: ".$comment;
-
-    }else{
-        echo "No comment found.";
     }
-//6
+
 ?>
 
 <!DOCTYPE html>
@@ -22,9 +30,10 @@
 <body style="text-align: center;">
 
     <form method="POST" action="">
-        <input type="text" name="username" placeholder="User Name">
+        <br>
+        <input type="text" name="username" placeholder="User Name" value="<?php echo $username; ?>">
         <br><br>
-        <textarea name="comment" id="" cols="30" rows="10" placeholder="Write your comment here..." ></textarea>
+        <textarea name="comment" id="" cols="30" rows="10" placeholder="Write your comment here..." > <?php echo $comment; ?></textarea>
         <br><br>
         <input type="submit" value="Share Comment">
     </form>
